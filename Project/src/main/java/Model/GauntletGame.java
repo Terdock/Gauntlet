83443f8ae94cdd0a82a4e713c66764b32e2abe8d
Kass <Kass@ulb.ac.be>
2016-04-17 02:00:43 +0200

    j'ai aggrandi la taille

--- Project/src/main/java/Model/GauntletGame.java
+++ Project/src/main/java/Model/GauntletGame.java
@@ -6,10 +6,10 @@ import observer.Observable;
 public class GauntletGame extends AbstractModel {
 	
 	private Integer numberMap; 
-	private static Integer nombreLigne = 15;
+	private static Integer nombreLigne = 100;
 	
 
-	private static Integer nombreColonne = 15;
+	private static Integer nombreColonne = 100;
 	IPlateau plateau;
 	private ArrayList<Hero> List_Hero = new ArrayList<Hero>();
 	private ArrayList<WorldEntity> listHero = new ArrayList<WorldEntity>();
