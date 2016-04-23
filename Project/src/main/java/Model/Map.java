0ef38cb5bb87fb38669a5660697e7833af6198e1
Kass <Kass@ulb.ac.be>
2016-04-23 22:02:19 +0200

    modification de la map

--- Project/src/main/java/Model/Map.java
+++ Project/src/main/java/Model/Map.java
@@ -10,7 +10,6 @@ public class Map implements IMap {
 	private Integer NumberOfDoor;
 	private Integer numberMap;
 	private ArrayList<PlateauObject> listDoor;
-	private ArrayList<PlateauObject> listTerrainArene = new ArrayList<PlateauObject>();
 	private ArrayList<PlateauObject> listWallMap = new ArrayList<PlateauObject>();
 	private PlateauObject staircaseMap;
 	private PlateauObject holeMap;
@@ -45,158 +44,35 @@ public class Map implements IMap {
 	}
 
 
-	public ArrayList<PlateauObject> getListDoor() {
-		return listDoor;
-	}
-
-
-	public void setListDoor(ArrayList<PlateauObject> listDoor) {
-		this.listDoor = listDoor;
-	}
-	
-	
-	
-	
-	/* (non-Javadoc)
-	 * @see Model.IMap#createListTerrain(java.lang.Integer, java.lang.Integer, Model.PlateauObject[][])
-	 */
 	@Override
 	public PlateauObject[][] createListTerrain(Integer nombreLigne, Integer nombreColonne,PlateauObject[][] listTerrain){
-		for (Integer posY = 0; posY <= nombreLigne + 1; posY++){
-			for (Integer posX = 0; posX<= nombreColonne + 1; posX++){
-				if (posX.equals(0) || posY.equals(0) || posX.equals(nombreColonne + 1) || posY.equals(nombreLigne + 1)){
-					//listTerrain.add(new Wall(posX*width, posY*height, false, numberMap));
-				}
-				else {
-					//listTerrain.add(new Sol(posX*width, posY*height, true, numberMap));
-				}
-			}	
-		}
-		return listTerrain;
-	}
-
-	public ArrayList<PlateauObject> create_MAP0(){
-		listDoor = new ArrayList<PlateauObject>(); 
-		ArrayList<PlateauObject> list  = new ArrayList<PlateauObject>();
-		Integer i= 0;
+		Integer i = 0;
 		for(Integer h = 0; h < 5; h++){
-			for(Integer y = 0; y < nombreColonne+1; y++){
-				for(Integer x = 0; x < nombreLigne+1; x++){
-					if(x.equals(20) && y.equals(10)){
+			for (Integer posY = 0; posY <= nombreLigne + 1; posY++){
+				for (Integer posX = 0; posX <= nombreColonne + 1; posX++){
+					listTerrain[posX][posY] = new Sol(posX*Plateau.getWidth(), posY*Plateau.getWidth(), true, numberMap);
+					
+					if (checkInDoor( posX, posY, h)){
+						listTerrain[posX][posY] = new Door(posX*Plateau.getWidth(),posY*Plateau.getHeight(),false,i);
+						i++;
+					}	
+					else if (checkInWall(posX, posY, h)){
 						
 					}
-					else if (checkInDoor( x, y, h)){
-						listDoor.add(new Door(x*Plateau.getWidth(),y*Plateau.getHeight(),false,i));i++;						
+					else if( posX == h*20 || posY == h*20 ){
+						listTerrain[posX][posY] = new Wall(posX*Plateau.getWidth(),posY*Plateau.getHeight(),false, 0);
 					}
-					else if( x == h*20 || y == h*20 ){
-						list.add(new Wall(x*Plateau.getWidth(),y*Plateau.getHeight(),false, 0));
-						if(checkInWall( x, y, h)){
-							list.get(list.size()-1).setForm(6);
-						}
-						else if (x.equals(h*20)){
-							list.get(list.size()-1).setForm(1);
-						}
-						else{
-							list.get(list.size()-1).setForm(0);	
-						}
-					}	
-				}
-			}
+					else if (posX.equals(0) || posY.equals(0) || posX.equals(nombreColonne + 1) || posY.equals(nombreLigne + 1)){
+						listTerrain[posX][posY] = new Wall(posX*Plateau.getWidth(), posY*Plateau.getWidth(), false, numberMap);
+					}
+				}	
+			}	
 		}
-		this.NumberOfDoor = i;
-		return list;
+		NumberOfDoor = i;
+		return listTerrain;
 	}
 	
-	public ArrayList<PlateauObject> create_MAP1(){
-		listDoor = new ArrayList<PlateauObject>(); 
-		ArrayList<PlateauObject> list  = new ArrayList<PlateauObject>();
-		Integer i= 0;
-		for(Integer h = 0; h < 5; h++){
-			for(Integer y = 0; y < nombreColonne+1; y++){
-				for(Integer x = 0; x < nombreLigne+1; x++){
-					if (checkInDoor( x, y, h)){
-						listDoor.add(new Door(x*Plateau.getWidth(),y*Plateau.getHeight(),false,i));i++;}
-					
-					else if( x == h*20 || y == h*20 ){
-						list.add(new Wall(x*Plateau.getWidth(),y*Plateau.getHeight(),false, 0));
-					}
-					else if( x == h*20 || y == h*20 ){
-						list.add(new Wall(x*Plateau.getWidth(),y*Plateau.getHeight(),false, 0));
-					}
-				}
-			}
-		}
-		this.NumberOfDoor = i;
-		return list;
-	}
-	public ArrayList<PlateauObject> create_MAP2(){
-		listDoor = new ArrayList<PlateauObject>(); 
-		ArrayList<PlateauObject> list  = new ArrayList<PlateauObject>();
-		Integer i= 0;
-		for(Integer h = 0; h < 5; h++){
-			for(Integer y = 0; y < nombreColonne+1; y++){
-				for(Integer x = 0; x < nombreLigne+1; x++){
-					if (checkInDoor( x, y, h)){
-							listDoor.add(new Door(x*Plateau.getWidth(),y*Plateau.getHeight(),false,i));i++;
-							}
-					
-					else if( x == h*20 || y == h*20 ){
-						list.add(new Wall(x*Plateau.getWidth(),y*Plateau.getHeight(),false, 0));
-					}
-					else if( x == h*20 || y == h*20 ){
-						list.add(new Wall(x*Plateau.getWidth(),y*Plateau.getHeight(),false, 0));
-					}
-				}
-			}
-		}
-		this.NumberOfDoor = i;
-		return list;
-	}
-	public ArrayList<PlateauObject> create_MAP3(){
-		listDoor = new ArrayList<PlateauObject>(); 
-		ArrayList<PlateauObject> list  = new ArrayList<PlateauObject>();
-		Integer i= 0;
-		for(Integer h = 0; h < 5; h++){
-			for(Integer y = 0; y < nombreColonne+1; y++){
-				for(Integer x = 0; x < nombreLigne+1; x++){
-					if (checkInDoor( x, y, h)){
-						listDoor.add(new Door(x*Plateau.getWidth(),y*Plateau.getHeight(),false,i));i++;}
-					
-					else if( x == h*20 || y == h*20 ){
-						list.add(new Wall(x*Plateau.getWidth(),y*Plateau.getHeight(),false, 0));
-					}
-					else if( x == h*20 || y == h*20 ){
-						list.add(new Wall(x*Plateau.getWidth(),y*Plateau.getHeight(),false, 0));
-					}
-				}
-			}
-		}
-		this.NumberOfDoor = i;
-		return list;
-		}
 	
-	public ArrayList<PlateauObject> create_MAP4(){
-		listDoor = new ArrayList<PlateauObject>(); 
-		ArrayList<PlateauObject> list  = new ArrayList<PlateauObject>();
-		Integer i= 0;
-		for(Integer h = 0; h < 5; h++){
-			for(Integer y = 0; y < nombreColonne+1; y++){
-				for(Integer x = 0; x < nombreLigne+1; x++){					
-					if (checkInDoor( x, y, h)){
-						listDoor.add(new Door(x*Plateau.getWidth(),y*Plateau.getHeight(),false,i));i++;
-					}
-					
-					else if( x == h*20 || y == h*20 ){
-						list.add(new Wall(x*Plateau.getWidth(),y*Plateau.getHeight(),false, 0));
-					}
-				}
-			}
-		}
-		this.NumberOfDoor = i;
-		return list;
-		}
-	
-
 	public ArrayList<PlateauObject> create_Door(){
 		return listDoor ;
 	}
@@ -397,36 +273,6 @@ public class Map implements IMap {
 	}
 	
 	
-	
-	private void create_MAP(){
-		staircaseMap = mapParLevel.create_Staircase0();
-		if(numberMap == 0){
-			this.listWallMap = mapParLevel.create_MAP0();
-			this.holeMap = mapParLevel.create_Hole0();
-			this.listMonster = mapParLevel.create_Monster0();
-		}
-		else if(numberMap == 1){
-			this.listWallMap = mapParLevel.create_MAP1();
-			this.holeMap = mapParLevel.create_Hole1();
-			this.listMonster = mapParLevel.create_Monster1();
-		}
-		else if(numberMap == 2){
-			this.listWallMap = mapParLevel.create_MAP2();
-		 	this.holeMap = mapParLevel.create_Hole2();
-		 	this.listMonster = mapParLevel.create_Monster2();
-		}
-		 else if(numberMap == 3){
-			listWallMap = mapParLevel.create_MAP3();
-			holeMap = mapParLevel.create_Hole3();
-			this.listMonster = mapParLevel.create_Monster3();
-		 }
-		else if(numberMap == 4){
-			listWallMap = mapParLevel.create_MAP4();
-			holeMap = mapParLevel.create_Hole4();
-			this.listMonster = mapParLevel.create_Monster4();
-		}
-		listDoor = mapParLevel.create_Door();
-	}
 
 	
 	/**
@@ -435,36 +281,6 @@ public class Map implements IMap {
 	 */
 	
 	
-	private void wallReplaceGround(){
-		for (PlateauObject wallMap : listWallMap){
-				Integer posX = wallMap.getPosX();
-				Integer posY = wallMap.getPosY();
-				listTerrain.set(indiceTerrain(posX,posY), wallMap);
-			}
-		}
-		
-	private void doorReplaceGround(){
-		for (PlateauObject door : listDoor){
-				Integer posX = door.getPosX();
-				Integer posY = door.getPosY();
-				listTerrain.set(indiceTerrain(posX,posY), door);
-			}
-	}
-	
-	private void Staircase_replace_Wall(){
-		Integer posX = staircaseMap.getPosX();
-		Integer posY = staircaseMap.getPosY();
-		listTerrain.set(indiceTerrain(posX,posY),staircaseMap);
-	}
-	
-	
-	private void Hole_replace_Wall(){
-		Integer posX = holeMap.getPosX();
-		Integer posY = holeMap.getPosY();
-		listTerrain.set(indiceTerrain(posX,posY),holeMap);
-	}
-	
-
 
 
 }
